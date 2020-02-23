1012905bd1a1799aceaebfc04f2f5f004da0ddaa
Emil Gadzhiyev <57042426+emilgadzhiyev@users.noreply.github.com>
2020-02-23 10:30:58 +0400

    Update storyteller.cs

--- storyteller.cs
+++ storyteller.cs
@@ -1,1 +1,78 @@
-
+using System;
+namespace StoryTeller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* THE MYSTERIOUS NOISE */
+            // Start by asking for the user's name:
+            Console.Write("What is your name?: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}! Welcome to our story.");
+            Console.WriteLine("It begins on a cold rainy night. You're sitting in your room and hear a noise coming from down the hall. Do you go investigate?");
+            Console.Write("Type YES or NO: ");
+            string noiseChoise = Console.ReadLine();
+            noiseChoise = noiseChoise.ToUpper();
+            Console.WriteLine(noiseChoise);
+            if (noiseChoise == "NO")
+            {
+                Console.WriteLine("Not much of an adventure if we don't leave our room!");
+                Console.WriteLine("THE END");
+            }
+            else if (noiseChoise == "YES")
+            {
+                Console.WriteLine("You walk into the hallway and see a light coming from under a door down the hall.");
+                Console.WriteLine("You walk towards it. Do you open it or knock?");
+            }
+            Console.Write("Type OPEN or KNOCK: ");
+            string doorChoise = Console.ReadLine();
+            doorChoise = doorChoise.ToUpper();
+            Console.WriteLine(doorChoise);
+            if (doorChoise == "KNOCK")
+            {
+                Console.WriteLine("A voice behind the door speaks. It says, \"Answer this riddle: \"");
+                Console.WriteLine("\"Poor people have it. Rich people need it. If you eat it you die. What is it?\"");
+                Console.Write("Type your answer: ");
+                string riddleAnswer = Console.ReadLine();
+                riddleAnswer = riddleAnswer.ToUpper();
+                // Console.WriteLine(riddleAnswer);
+                if (riddleAnswer == "NOTHING")
+                {
+                    Console.WriteLine("The door opens and NOTHING is there.");
+                    Console.WriteLine("You turn off the light and run back to your room and lock the door");
+                    Console.WriteLine("THE END.");
+                }
+                else
+                {
+                    Console.WriteLine("You answered incorrectly. The door doesn't open.");
+                    Console.WriteLine("THE END.");
+                }
+            }
+            else if (doorChoise == "OPEN")
+            {
+                Console.WriteLine("The door is locked! See if one of your three keys will open it.");
+                Console.Write("Enter a number (1-3): ");
+                string keyChoise = Console.ReadLine();
+                keyChoise = keyChoise.ToUpper();
+                // Console.WriteLine(keyChoise);
+                switch (keyChoise)
+                {
+                    case "1":
+                        Console.WriteLine("You choose the first key. Lucky choise!");
+                        Console.WriteLine("The door opens and NOTHING is there. Strange...");
+                        Console.WriteLine("THE END");
+                        break;
+                    case "2":
+                        Console.WriteLine("You choose the second key. The door doesn't open.");
+                        Console.WriteLine("THE END");
+                        break;
+                    case "3":
+                        Console.WriteLine("You choose the third key. The door doesn't open.");
+                        Console.WriteLine("THE END");
+                        break;
+                }
+            }
+        }
+    }
+}
